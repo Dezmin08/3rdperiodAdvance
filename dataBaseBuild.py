795bf774f5aca64f43599382f906943d6b318e86
dezmin <60934807+Dezmin08@users.noreply.github.com>
2020-02-12 12:17:08 -0500

    Docstring updated by dez
    i updated the code on line 2

--- dataBaseBuild.py
+++ dataBaseBuild.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+'''we had 5 solutions and we came down to two solutions'''
 studentId = str(input("Enter your ID here: ")) #students input there student id numbers
 now = str(datetime.now())
 if studentId in ("110255","112266","123987","212901"): #this is my database of students
